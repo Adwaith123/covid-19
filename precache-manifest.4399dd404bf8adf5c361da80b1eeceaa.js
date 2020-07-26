c15dab8e59f3cd8be40575dbba24f290db7c2d54
== <>
2020-07-26 20:33:55 +0530

    Updates

---
+++ precache-manifest.4399dd404bf8adf5c361da80b1eeceaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9591b3e9557f635fbcb15dd8f3e5b0a",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "a1981fd5524075da41bb",
+    "url": "/covid-19/static/css/main.3ce645da.chunk.css"
+  },
+  {
+    "revision": "78c255335ce1977db9ca",
+    "url": "/covid-19/static/js/2.f851f0e3.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid-19/static/js/2.f851f0e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1981fd5524075da41bb",
+    "url": "/covid-19/static/js/main.e1e0b59a.chunk.js"
+  },
+  {
+    "revision": "ce18e44a6f281162c1d1",
+    "url": "/covid-19/static/js/runtime-main.2c53739b.js"
+  },
+  {
+    "revision": "22f5a4e90741d447dccf591973434802",
+    "url": "/covid-19/static/media/logo.22f5a4e9.png"
+  }
+]);
